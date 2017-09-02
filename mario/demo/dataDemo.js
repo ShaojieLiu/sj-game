3dd2fc7ec31810e3eaa77d14803a61b895e7c377
刘少杰 <liushaojie@cvte.com>
2017-09-02 11:28:39 +0800

    mario资源

--- mario/demo/dataDemo.js
+++ mario/demo/dataDemo.js
@@ -1,8 +1,16 @@
 const __start = () => {
     drawPage('#canvas')
     drawMario('#mario')
+    bindDiy('#mario')
     drawDiy('#diy1', 2, 4)
+    drawDiy('#diy2', 2, 2)
+    drawDiy('#diy3', 4, 2)
     // drawWalking('#mario-walking')
+}
+
+const __update = () => {
+    drawPage('#canvas')
+    drawMario('#mario')
 }
 
 const __main = () => {
